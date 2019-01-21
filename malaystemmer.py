2812364c58c109c1fd392d25099ccd00c93c5bd5
Ili Fadhilah <ilifadhilah@hotmail.com>
2019-01-21 22:17:18 +0800

    Add files via upload

---
+++ malaystemmer.py
@@ -0,0 +1,20 @@
+
+from functions import replace_last, m_Prefix, p_Prefix, di_Prefix, ke_Prefix, ter_Prefix, ber_Prefix, Suffix, Verify
+
+def malaystemmer(text):
+    if text[0] + text[1] == 'pe':
+        stemmedword = p_Prefix(text)
+    elif text[0] + text[1] == 'me':
+        stemmedword = m_Prefix(text)
+    elif text[0] + text[1] == 'di':
+        stemmedword = di_Prefix(text)
+    elif text[0] + text[1] == 'ke':
+        stemmedword = ke_Prefix(text)
+    elif text[0] + text[1] + text[2] == 'ter':
+        stemmedword = ter_Prefix(text)
+    elif text[0] + text[1] + text[2] == 'ber':
+        stemmedword = ber_Prefix(text)
+    else:
+        stemmedword = p_Prefix(text)
+        
+    return stemmedword
